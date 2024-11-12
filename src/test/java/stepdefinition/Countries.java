3aec36c6415918de34f0d4966ffcf2019e76898f
Mthembu <sboniso.mthembu@accenture.com>
2024-11-12 11:20:20 +0200

    made changes on the countries class to verify the language from the response

--- src/test/java/stepdefinition/Countries.java
+++ src/test/java/stepdefinition/Countries.java
@@ -7,17 +7,14 @@ import io.restassured.RestAssured;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
-import org.hamcrest.Matchers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Countries {
@@ -25,7 +22,6 @@ public class Countries {
     RequestSpecification httprequest;
     Response response;
     int responseCode;
-    ResponseBody body;
 
     @Given("I hit the url get rest countries api endpoint")
     public void i_hit_the_url_get_rest_countries_api_endpoint() {
@@ -40,49 +36,49 @@ public class Countries {
         responseCode = response.statusCode();
     }
 
-    @Then("I verify the status code as {int}")
-    public void i_verify_the_status_code_as(int expectedStatusCode) {
-//        responseCode = response.statusCode();
+    @Then("I verify the schema")
+    public void i_verify_the_schema() {
 
         String responseBody = response.getBody().asString();
 
-        assertEquals(responseCode, expectedStatusCode);
+        assertEquals(responseCode, 200);
         assertThat(responseBody, JsonSchemaValidator.matchesJsonSchemaInClasspath("restCountriesSchema.json"));
-//        assertEquals(responseCode, expectedStatusCode);
     }
 
     @Then("Verify that there are {int} countries")
     public void verify_that_there_are_countries(Integer expectedNumberOfCountries) {
-        body = response.getBody();
 
         //JSON representation from response body
         JsonPath jsonPath = response.jsonPath();
 
-        String numberOfCountries = jsonPath.getJsonObject("name.common").toString();
-        System.out.println( numberOfCountries.length());
-        String[] allCountries = (numberOfCountries.split(","));
-//        System.out.println(allCountries[0]);
-//        System.out.println(allCountries.length);
+        ArrayList<String> listOfCountries = jsonPath.getJsonObject("name.common");
+
         assertEquals(responseCode, 200);
-        assertEquals(expectedNumberOfCountries, allCountries.length);
+        assertEquals(expectedNumberOfCountries, listOfCountries.size());
     }
 
-    @When("I pass the url {} of a specific country in the request")
+    @When("I pass the url {string} of a specific country in the request")
     public void i_pass_the_url_of_south_africa_in_the_request(String country) {
+
         httprequest = given().pathParam("country", country);
         response = httprequest.get("v3.1/name/{country}");
 
         responseCode = response.statusCode();
     }
 
-    @Then("Verify that there {} is included")
-    public void verify_that_there_sasl_is_included(String expectedLanguage) {
+    @Then("Verify that the abbreviation {string} and language {string} is included")
+    public void verify_that_there_sasl_is_included(String expectedLanguageAbbr, String expectedLanguageName) {
+
+        String langFullName = expectedLanguageAbbr+"="+expectedLanguageName;
+        assertEquals(responseCode, 200);
+
         //JSON representation from response body
         JsonPath jsonPath = response.jsonPath();
 
-        assertEquals(responseCode, 200);
-//        ArrayList<String> sl = jsonPath.getJsonObject("languages");
-        assertThat(jsonPath.getJsonObject("languages"), hasItem(expectedLanguage));
+        ArrayList<LinkedHashMap<String, String>> languageList = new ArrayList<>(jsonPath.getList("languages"));
 
+        LinkedHashMap<String, String> languagesMap = languageList.get(0);
+
+        assertTrue("Expected the map to contain "+langFullName, languagesMap.containsKey(expectedLanguageAbbr) && expectedLanguageName.equals(languagesMap.get(expectedLanguageAbbr)));
     }
 }
--- src/test/resources/features/restCountries.feature
+++ src/test/resources/features/restCountries.feature
@@ -4,7 +4,7 @@
     Scenario: Ensure correct API endpoint schema
       Given I hit the url get rest countries api endpoint
       When I pass url of all countries in the request
-      Then I verify the status code as 200
+      Then I verify the schema
 
     Scenario: Confirmation of Countries
       Given I hit the url get rest countries api endpoint
@@ -13,8 +13,8 @@
 
     Scenario Outline: Validate Languages
       Given I hit the url get rest countries api endpoint
-      When I pass the url <specific country> of a specific country in the request
-      Then Verify that there <language> is included
+      When I pass the url "<specific country>" of a specific country in the request
+      Then Verify that the abbreviation "<abr_language>" and language "<full_name_language>" is included
       Examples:
-      |specific country| language|
-      |south africa    | sasl   |
+      |specific country| abr_language|full_name_language|
+      |south africa    | sasl         |South African Sign Language|
